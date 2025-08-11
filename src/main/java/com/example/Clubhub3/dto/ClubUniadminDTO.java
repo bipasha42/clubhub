f7904bfa83c9dd03f49a6867f5dbc7e29b48fa81
TASNIMA TABASSUM BIPASHA <tasnima.bipasha@gmail.com>
2025-08-11 17:39:39 +0600

    SCRUM-19-Club-admin-showcase-and-editB

--- src/main/java/com/example/Clubhub3/dto/ClubUniadminDTO.java
+++ src/main/java/com/example/Clubhub3/dto/ClubUniadminDTO.java
@@ -2,6 +2,7 @@ package com.example.Clubhub3.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.*;
 
 import java.util.UUID;
 
