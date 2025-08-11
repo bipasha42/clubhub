9e45a41b3e3d842187a746d429e75c98dd1a0cec
Refayet <13129292+Refayatul@users.noreply.github.com>
2025-08-11 22:59:28 +0600

    Initializing main branch

---
+++ src/test/java/com/example/Clubhub3/Clubhub3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Clubhub3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Clubhub3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
